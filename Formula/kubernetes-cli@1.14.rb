ce97839d1ab304d8077dc1e511ea0f5d900893c8
Cameron Boulton <cameronboulton@gmail.com>
2019-07-23 09:46:42 -0700

    Update kubernetes-cli@1.14.rb

--- Formula/kubernetes-cli@1.14.rb
+++ Formula/kubernetes-cli@1.14.rb
@@ -1,4 +1,4 @@
-class KubernetesCli < Formula
+class KubernetesCliAT114 < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
