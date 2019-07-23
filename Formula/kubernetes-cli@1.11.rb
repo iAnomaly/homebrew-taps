2a11b71328d2f7f6710c0bc19d4fa60f6901845a
Cameron Boulton <cameronboulton@gmail.com>
2019-07-23 10:30:05 -0700

    Create kubernetes-cli@1.11.rb

---
+++ Formula/kubernetes-cli@1.11.rb
@@ -0,0 +1,59 @@
+class KubernetesCliAT111 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      :tag => "v1.11.3",
+      :revision => "a4529464e4629c21224b3d52edfe0ea91b072862"
+  head "https://github.com/kubernetes/kubernetes.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d00a240d03a1de64df9d76df58855385638d0abdfdad09d2f88c2140a6178d75" => :mojave
+    sha256 "e1859f9f893237aa977be328846481b362e147ae92c2cde3e65a7d445b02dae9" => :high_sierra
+    sha256 "5752d5baff35437176ac169469fdc27e2bcb740da74ebbf3191ddbb009c23941" => :sierra
+    sha256 "67e7ad04098e52028d5d9e8f32fe9c530778be555842c6cf1ad39839945d312b" => :el_capitan
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    dir = buildpath/"src/k8s.io/kubernetes"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      # Race condition still exists in OSX Yosemite
+      # Filed issue: https://github.com/kubernetes/kubernetes/issues/34635
+      ENV.deparallelize { system "make", "generated_files" }
+
+      # Make binary
+      system "make", "kubectl"
+      bin.install "_output/local/bin/darwin/#{arch}/kubectl"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/kubectl completion bash")
+      (bash_completion/"kubectl").write output
+
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/kubectl completion zsh")
+      (zsh_completion/"_kubectl").write output
+
+      prefix.install_metafiles
+
+      # Install man pages
+      # Leave this step for the end as this dirties the git tree
+      system "hack/generate-docs.sh"
+      man1.install Dir["docs/man/man1/*.1"]
+    end
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output if build.stable?
+  end
+end
